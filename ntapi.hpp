cc449bf82d4d342d2fcc5654a4330b58d18fc917
henrypp <support@henrypp.org>
2019-09-25 16:22:08 +0600

    upd ntapi

--- ntapi.hpp
+++ ntapi.hpp
@@ -585,6 +585,218 @@ typedef struct _PROCESS_DEVICEMAP_INFORMATION_EX
     (p)->SecurityQualityOfService = NULL; \
     }
 
+// Kernel-user shared data
+
+typedef enum _NT_PRODUCT_TYPE
+{
+	NtProductWinNt = 1,
+	NtProductLanManNt,
+	NtProductServer
+} NT_PRODUCT_TYPE, *PNT_PRODUCT_TYPE;
+
+typedef enum _ALTERNATIVE_ARCHITECTURE_TYPE
+{
+	StandardDesign,
+	NEC98x86,
+	EndAlternatives
+} ALTERNATIVE_ARCHITECTURE_TYPE;
+
+#define PROCESSOR_FEATURE_MAX 64
+
+#define MAX_WOW64_SHARED_ENTRIES 16
+
+#define NX_SUPPORT_POLICY_ALWAYSOFF 0
+#define NX_SUPPORT_POLICY_ALWAYSON 1
+#define NX_SUPPORT_POLICY_OPTIN 2
+#define NX_SUPPORT_POLICY_OPTOUT 3
+
+#include <pshpack4.h>
+typedef struct _KSYSTEM_TIME
+{
+	ULONG LowPart;
+	LONG High1Time;
+	LONG High2Time;
+} KSYSTEM_TIME, *PKSYSTEM_TIME;
+
+#pragma warning(push)
+#pragma warning(disable: 4201)
+typedef struct _KUSER_SHARED_DATA
+{
+	ULONG TickCountLowDeprecated;
+	ULONG TickCountMultiplier;
+
+	volatile KSYSTEM_TIME InterruptTime;
+	volatile KSYSTEM_TIME SystemTime;
+	volatile KSYSTEM_TIME TimeZoneBias;
+
+	USHORT ImageNumberLow;
+	USHORT ImageNumberHigh;
+
+	WCHAR NtSystemRoot[260];
+
+	ULONG MaxStackTraceDepth;
+
+	ULONG CryptoExponent;
+
+	ULONG TimeZoneId;
+	ULONG LargePageMinimum;
+	ULONG AitSamplingValue;
+	ULONG AppCompatFlag;
+	ULONGLONG RNGSeedVersion;
+	ULONG GlobalValidationRunlevel;
+	LONG TimeZoneBiasStamp;
+
+	ULONG NtBuildNumber;
+	NT_PRODUCT_TYPE NtProductType;
+	BOOLEAN ProductTypeIsValid;
+	UCHAR Reserved0[1];
+	USHORT NativeProcessorArchitecture;
+
+	ULONG NtMajorVersion;
+	ULONG NtMinorVersion;
+
+	BOOLEAN ProcessorFeatures[PROCESSOR_FEATURE_MAX];
+
+	ULONG Reserved1;
+	ULONG Reserved3;
+
+	volatile ULONG TimeSlip;
+
+	ALTERNATIVE_ARCHITECTURE_TYPE AlternativeArchitecture;
+	ULONG BootId;
+
+	LARGE_INTEGER SystemExpirationDate;
+
+	ULONG SuiteMask;
+
+	BOOLEAN KdDebuggerEnabled;
+	union
+	{
+		UCHAR MitigationPolicies;
+		struct
+		{
+			UCHAR NXSupportPolicy : 2;
+			UCHAR SEHValidationPolicy : 2;
+			UCHAR CurDirDevicesSkippedForDlls : 2;
+			UCHAR Reserved : 2;
+		};
+	};
+
+	USHORT CyclesPerYield;
+
+	volatile ULONG ActiveConsoleId;
+
+	volatile ULONG DismountCount;
+
+	ULONG ComPlusPackage;
+
+	ULONG LastSystemRITEventTickCount;
+
+	ULONG NumberOfPhysicalPages;
+
+	BOOLEAN SafeBootMode;
+	UCHAR VirtualizationFlags;
+	UCHAR Reserved12[2];
+
+	union
+	{
+		ULONG SharedDataFlags;
+		struct
+		{
+			ULONG DbgErrorPortPresent : 1;
+			ULONG DbgElevationEnabled : 1;
+			ULONG DbgVirtEnabled : 1;
+			ULONG DbgInstallerDetectEnabled : 1;
+			ULONG DbgLkgEnabled : 1;
+			ULONG DbgDynProcessorEnabled : 1;
+			ULONG DbgConsoleBrokerEnabled : 1;
+			ULONG DbgSecureBootEnabled : 1;
+			ULONG DbgMultiSessionSku : 1;
+			ULONG DbgMultiUsersInSessionSku : 1;
+			ULONG DbgStateSeparationEnabled : 1;
+			ULONG SpareBits : 21;
+		};
+	};
+	ULONG DataFlagsPad[1];
+
+	ULONGLONG TestRetInstruction;
+	LONGLONG QpcFrequency;
+	ULONG SystemCall;
+	ULONG SystemCallPad0;
+	ULONGLONG SystemCallPad[2];
+
+	union
+	{
+		volatile KSYSTEM_TIME TickCount;
+		volatile ULONG64 TickCountQuad;
+		ULONG ReservedTickCountOverlay[3];
+	};
+	ULONG TickCountPad[1];
+
+	ULONG Cookie;
+	ULONG CookiePad[1];
+
+	LONGLONG ConsoleSessionForegroundProcessId;
+	ULONGLONG TimeUpdateLock;
+	ULONGLONG BaselineSystemTimeQpc;
+	ULONGLONG BaselineInterruptTimeQpc;
+	ULONGLONG QpcSystemTimeIncrement;
+	ULONGLONG QpcInterruptTimeIncrement;
+	UCHAR QpcSystemTimeIncrementShift;
+	UCHAR QpcInterruptTimeIncrementShift;
+
+	USHORT UnparkedProcessorCount;
+	ULONG EnclaveFeatureMask[4];
+
+	ULONG TelemetryCoverageRound;
+
+	USHORT UserModeGlobalLogger[16];
+	ULONG ImageFileExecutionOptions;
+
+	ULONG LangGenerationCount;
+	ULONGLONG Reserved4;
+	volatile ULONG64 InterruptTimeBias;
+	volatile ULONG64 QpcBias;
+
+	ULONG ActiveProcessorCount;
+	volatile UCHAR ActiveGroupCount;
+	UCHAR Reserved9;
+	union
+	{
+		USHORT QpcData;
+		struct
+		{
+			UCHAR QpcBypassEnabled : 1;
+			UCHAR QpcShift : 1;
+		};
+	};
+
+	LARGE_INTEGER TimeZoneBiasEffectiveStart;
+	LARGE_INTEGER TimeZoneBiasEffectiveEnd;
+	XSTATE_CONFIGURATION XState;
+} KUSER_SHARED_DATA, *PKUSER_SHARED_DATA;
+#pragma warning(pop)
+#include <poppack.h>
+
+C_ASSERT (FIELD_OFFSET (KUSER_SHARED_DATA, TickCountMultiplier) == 0x4);
+C_ASSERT (FIELD_OFFSET (KUSER_SHARED_DATA, InterruptTime) == 0x8);
+C_ASSERT (FIELD_OFFSET (KUSER_SHARED_DATA, SystemTime) == 0x14);
+C_ASSERT (FIELD_OFFSET (KUSER_SHARED_DATA, NtSystemRoot) == 0x30);
+C_ASSERT (FIELD_OFFSET (KUSER_SHARED_DATA, LargePageMinimum) == 0x244);
+C_ASSERT (FIELD_OFFSET (KUSER_SHARED_DATA, NtProductType) == 0x264);
+C_ASSERT (FIELD_OFFSET (KUSER_SHARED_DATA, NtMajorVersion) == 0x26c);
+C_ASSERT (FIELD_OFFSET (KUSER_SHARED_DATA, NtMinorVersion) == 0x270);
+C_ASSERT (FIELD_OFFSET (KUSER_SHARED_DATA, ProcessorFeatures) == 0x274);
+C_ASSERT (FIELD_OFFSET (KUSER_SHARED_DATA, KdDebuggerEnabled) == 0x2d4);
+C_ASSERT (FIELD_OFFSET (KUSER_SHARED_DATA, ActiveConsoleId) == 0x2d8);
+C_ASSERT (FIELD_OFFSET (KUSER_SHARED_DATA, NumberOfPhysicalPages) == 0x2e8);
+C_ASSERT (FIELD_OFFSET (KUSER_SHARED_DATA, SafeBootMode) == 0x2ec);
+C_ASSERT (FIELD_OFFSET (KUSER_SHARED_DATA, TickCount) == 0x320);
+C_ASSERT (FIELD_OFFSET (KUSER_SHARED_DATA, TickCountQuad) == 0x320);
+C_ASSERT (FIELD_OFFSET (KUSER_SHARED_DATA, XState) == 0x3d8);
+
+#define USER_SHARED_DATA ((KUSER_SHARED_DATA * const)0x7ffe0000)
+
 extern "C" {
 	NTSYSCALLAPI
 		NTSTATUS
