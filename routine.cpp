19fa8c56395faac4d7d0b2b4a9536718d982d3a7
henrypp <support@henrypp.org>
2019-11-05 12:37:49 +0600

    fix table text priority

--- routine.cpp
+++ routine.cpp
@@ -3595,11 +3595,11 @@ void _r_ctrl_settabletext (HWND hwnd, INT ctrl_id1, LPCWSTR text1, INT ctrl_id2,
 	const INT wnd_spacing = rc_ctrl.left;
 	const INT wnd_width = _R_RECT_WIDTH (&rc_wnd) - (wnd_spacing * 2);
 
-	const INT text1_width = _r_dc_fontwidth (hdc1, text1, INVALID_SIZE_T);
-	const INT text2_width = _r_dc_fontwidth (hdc2, text2, INVALID_SIZE_T);
+	INT ctrl1_width = _r_dc_fontwidth (hdc1, text1, INVALID_SIZE_T);
+	INT ctrl2_width = _r_dc_fontwidth (hdc2, text2, INVALID_SIZE_T);
 
-	const INT ctrl1_width = (std::min) (text1_width, wnd_width - text2_width - wnd_spacing);
-	const INT ctrl2_width = (std::min) (text2_width, wnd_width - text1_width - wnd_spacing);
+	ctrl2_width = (std::min) (ctrl2_width, wnd_width - ctrl1_width - wnd_spacing);
+	ctrl1_width = (std::min) (ctrl1_width, wnd_width - ctrl2_width - wnd_spacing); // note: changed order for correct priority!
 
 	SetWindowText (hctrl1, text1);
 	SetWindowText (hctrl2, text2);
