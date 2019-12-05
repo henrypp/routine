cf92c445b766e8d76ace6b1757616a14efa479e6
henrypp <support@henrypp.org>
2019-12-05 19:56:50 +0600

    cleanup

--- rconfig.hpp
+++ rconfig.hpp
@@ -12,7 +12,6 @@
 
 //#define _APP_BETA // app has pre-release status
 
-//#define _APP_NO_ABOUT // disable about dialog feature
 //#define _APP_NO_GUEST // disable "guest" mode, running without admin rights; see _APP_HAVE_SKIPUAC
 //#define _APP_NO_WINXP // disable winxp support
 //#define _APP_NO_MUTEX // disable mutex
