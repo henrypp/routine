b0e9ce8f15570d32b109c83f3a869cb33e046c24
henrypp <support@henrypp.org>
2020-03-21 15:25:16 +0600

    use "__forceinline" for memory routine

--- routine.hpp
+++ routine.hpp
@@ -295,9 +295,17 @@ FORCEINLINE void* _r_mem_allocex (size_t bytes_count, DWORD flags)
 	return RtlAllocateHeap (rinternal::hProcessHeap, flags, bytes_count);
 }
 
-void* _r_mem_alloc (size_t bytes_count);
+FORCEINLINE void* _r_mem_alloc (size_t bytes_count)
+{
+	return _r_mem_allocex (bytes_count, HEAP_GENERATE_EXCEPTIONS);
+}
+
+FORCEINLINE void _r_mem_free (void* pmemory)
+{
+	RtlFreeHeap (rinternal::hProcessHeap, 0, pmemory);
+}
+
 void* _r_mem_realloc (void* pmemory, size_t bytes_count);
-void _r_mem_free (void* pmemory);
 
 /*
 	Objects reference
