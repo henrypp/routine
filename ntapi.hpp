636924962f7a0d92e9d9301465ac6d973d689797
henrypp <support@henrypp.org>
2020-09-05 20:38:41 +0600

    updated nt definitions

--- ntapi.hpp
+++ ntapi.hpp
@@ -1968,11 +1968,54 @@ NtReleaseSemaphore (
 );
 
 NTSYSCALLAPI
+BOOLEAN
+NTAPI
+RtlDispatchException (
+	_In_ PEXCEPTION_RECORD ExceptionRecord,
+	_In_ PCONTEXT ContextRecord
+);
+
+NTSYSCALLAPI
 DECLSPEC_NORETURN
 VOID
 NTAPI
 RtlRaiseStatus (
 	_In_ NTSTATUS Status
+);
+
+NTSYSCALLAPI
+VOID
+NTAPI
+RtlRaiseException (
+	_In_ PEXCEPTION_RECORD ExceptionRecord
+);
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtContinue (
+	_In_ PCONTEXT ContextRecord,
+	_In_ BOOLEAN TestAlert
+);
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtRaiseException (
+	_In_ PEXCEPTION_RECORD ExceptionRecord,
+	_In_ PCONTEXT ContextRecord,
+	_In_ BOOLEAN FirstChance
+);
+
+__analysis_noreturn
+NTSYSCALLAPI
+VOID
+NTAPI
+RtlAssert (
+	_In_ PVOID VoidFailedAssertion,
+	_In_ PVOID VoidFileName,
+	_In_ ULONG LineNumber,
+	_In_opt_ PSTR MutableMessage
 );
 
 // winbase:InitializeSRWLock
